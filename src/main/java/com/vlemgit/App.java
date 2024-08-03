36c242347afeaefef1a28d81d2709c7a7fa583f3
vlem <vlemGit@github.com>
2024-08-03 16:02:47 +0200

    listeners on textfield chages and save in the corresponded file

--- src/main/java/com/vlemgit/App.java
+++ src/main/java/com/vlemgit/App.java
@@ -6,28 +6,8 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-/**
- * JavaFX App
- */
+
 public class App extends Application {
-
-    /* private static Scene scene;
-
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 1200, 720);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    } */
 
     @Override
     public void start(Stage primaryStage) {
@@ -35,7 +15,7 @@ public class App extends Application {
             FXMLLoader loader = new FXMLLoader(App.class.getResource("primary.fxml"));
             BorderPane root = loader.load();
 
-            Scene scene = new Scene(root, 800, 600);
+            Scene scene = new Scene(root, 1200, 800);
             primaryStage.setScene(scene);
             primaryStage.setTitle("Property Editor");
             primaryStage.show();
--- src/main/java/com/vlemgit/PrimaryController.java
+++ src/main/java/com/vlemgit/PrimaryController.java
@@ -1,17 +1,24 @@
 package com.vlemgit;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
@@ -39,17 +46,47 @@ public class PrimaryController {
 
     private Path directoryFileSystemLocation;
 
+    private Path currentPropertyFile;
+
+    private Map<String, String> currentPropertyContent;
+
+    private List<String> originalFileLines = new ArrayList<>();
+
     @FXML
     private void chooseDirectoryHandler() {
         DirectoryChooser directoryChooser = new DirectoryChooser();
         directoryChooser.setTitle("Select Directory");
         File selectedDirectoryFile = directoryChooser.showDialog(chooseDirectoryButton.getScene().getWindow());
         if (selectedDirectoryFile != null) {
+            //clear window ? to avoid exception when we re choose directory
             directoryFileSystemLocation = selectedDirectoryFile.toPath();
             try {
                 displayDirectoryAsTreeView(directoryFileSystemLocation);
             } catch (IOException e) {
                 e.printStackTrace();
+            }
+        }
+    }
+
+    private void reloadPropertyFileContent(Path file) throws IOException{
+    Map<String, String> propertiesMap = fetchPropertyFileContent(file);
+    propertyEditorVBox.getChildren().clear();
+
+    for (Map.Entry<String, String> entry : propertiesMap.entrySet()) {
+        String key = entry.getKey();
+        String value = entry.getValue();
+        
+        TextField textField = new TextField(value);
+        textField.setId(key);
+        propertyEditorVBox.getChildren().add(textField);
+    }
+    }
+
+    private void savePropertiesToFile(Path file) throws IOException {
+        try (BufferedWriter writer = Files.newBufferedWriter(file)) {
+            for (Map.Entry<String, String> entry : this.currentPropertyContent.entrySet()) {
+                writer.write(entry.getKey().trim() + "=" + entry.getValue().trim());
+                writer.newLine();
             }
         }
     }
@@ -70,9 +107,10 @@ public class PrimaryController {
             if (newSelection != null && newSelection.isLeaf()) {
                 String selectedFileRelativePath = getFullPath(newSelection, rootItem);
                 Path fullPath = directoryFileSystemLocation.resolve(selectedFileRelativePath);
+                this.currentPropertyFile= fullPath;
                 try{
-                Map<String, String> propertyContent = fetchPropertyFileContent(fullPath);
-                loadFileProperties(propertyContent);
+                this.currentPropertyContent = fetchPropertyFileContent(fullPath);
+                loadFileProperties(this.currentPropertyContent);
                 } catch(IOException e){
                     e.printStackTrace();
                 }
@@ -118,36 +156,77 @@ public class PrimaryController {
     }
 
     private Map<String, String> fetchPropertyFileContent(Path file) throws IOException {
-        Properties properties = new Properties();
-        properties.load(Files.newBufferedReader(file));
+        Map<String, String> propertiesMapcontent = new LinkedHashMap<>();
+        
+        List<String> lines = Files.readAllLines(file);
+        
+        for (String line : lines) {
+            line = line.trim();
+            
+            if (!line.isEmpty() && line.contains("=")) {
+                String[] keyValue = line.split("=", 2);
+                
+                if (keyValue.length == 2) {
+                    String key = keyValue[0].trim();
+                    String value = keyValue[1].trim();                  
+                    propertiesMapcontent.put(key, value);
+                }
 
-        Map<String, String> propertiesMap = new HashMap<>();
-        for (String key : properties.stringPropertyNames()) {
-            propertiesMap.put(key, properties.getProperty(key));
+            }
         }
-        return propertiesMap;
+        return propertiesMapcontent;
     }
 
      private void loadFileProperties(Map<String, String> properties) {
         propertyEditorVBox.getChildren().clear();
 
-        if (properties != null) {
-            for (Map.Entry<String, String> entry : properties.entrySet()) {
-                HBox hBox = new HBox();
-                Label label = new Label(entry.getKey() + "=");
-                TextField textField = new TextField(entry.getValue());
-                //Button saveButton = new Button("Save");
+    if (properties != null) {
+        int rowIndex = 0;
+        for (Map.Entry<String, String> entry : properties.entrySet()) {
+            HBox hBox = new HBox();
+            hBox.setSpacing(25);
 
-                //saveButton.setOnAction(event -> saveProperty(filePath, entry.getKey(), textField.getText()));
-                //hBox.getChildren().addAll(label, textField, saveButton);
-                hBox.getChildren().addAll(label, textField);
-                propertyEditorVBox.getChildren().add(hBox);
+            TextField keyField = new TextField(entry.getKey());
+            keyField.setPrefWidth(250);
+            Label separatorLabel = new Label("=");
+            TextField valueField = new TextField(entry.getValue());
+            valueField.setPrefWidth(500);
+
+            if (rowIndex % 2 == 0) {
+                hBox.setStyle("-fx-background-color: #f0f0f0;");
+            } else {
+                hBox.setStyle("-fx-background-color: #ffffff;");
             }
+
+            keyField.textProperty().addListener((ObservableValue<? extends String> observable, String oldValue, String newValue) ->{
+                if (!newValue.trim().equals(oldValue.trim())) {
+                    String value = this.currentPropertyContent.remove(oldValue.trim());
+                    this.currentPropertyContent.put(newValue.trim(), value);
+                    try {
+                        System.out.println("old value : " +oldValue + " changed to => " + newValue);
+                        savePropertiesToFile(this.currentPropertyFile);
+                    } catch (IOException ex) {
+                    }
+                }
+                
+            });
+
+            valueField.textProperty().addListener((observable, oldValue, newValue) ->{
+                if (!newValue.trim().equals(oldValue.trim())) {
+                    String key = valueField.getId();
+                    this.currentPropertyContent.put(key, newValue.trim());
+                    try {
+                        System.out.println("old value : " +oldValue + " changed to => " + newValue);
+                        savePropertiesToFile(this.currentPropertyFile);
+                    } catch (IOException ex) {
+                    }
+                }   
+        });
+
+            hBox.getChildren().addAll(keyField,separatorLabel, valueField);
+            propertyEditorVBox.getChildren().add(hBox);
         }
     }
-
-    private void saveProperty(String filePath, String key, String value) {
-        System.out.println("Saving property " + key + "=" + value + " to file " + filePath);
     }
 
     private String getFullPath(TreeItem<String> item, TreeItem<String> rootItem) {
